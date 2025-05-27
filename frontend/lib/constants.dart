2f0700e34ff3df4d1b04a3a1c9ce1d694ce647ee
ashish <144021866+ashishexee@users.noreply.github.com>
2025-05-28 00:10:05 +0530

    app icon & name

--- frontend/lib/constants.dart
+++ frontend/lib/constants.dart
@@ -1,7 +1,7 @@
 const String baseUrl1 = 'http://192.168.29.229:3000';
-const String baseUrl2 = 'https://teledrive-6zuv.onrender.com';
+const String baseUrl2 = 'https://teledrive-3.onrender.com';
 
 class AppConstants {
-  static const String baseUrl = baseUrl1;
+  static const String baseUrl = baseUrl2;
   static const String botname = 'teledrive777_bot';
 }
